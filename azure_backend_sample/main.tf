b16f7da5cae33f53ec4927e9ef10bf3d1bd77d0f
ryo.takaki <sigaryoutakaki@gmail.com>
2020-12-29 14:04:02 +0900

    remote storeとして使うストレージアカウントを作成

---
+++ azure_backend_sample/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 2.26"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "terraform-backend-rg"
+  location = var.location
+}
+
+resource "azurerm_storage_account" "sa" {
+  name                     = "terraform"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  tags                     = var.tags
+
+}
