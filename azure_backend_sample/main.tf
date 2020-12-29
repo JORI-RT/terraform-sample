5e4b421a48e9a3a715817646bf677cceffecfb44
ryo.takaki <sigaryoutakaki@gmail.com>
2020-12-29 14:05:17 +0900

    name convention

--- azure_backend_sample/main.tf
+++ azure_backend_sample/main.tf
@@ -17,7 +17,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "sa" {
-  name                     = "terraform"
+  name                     = "terraformryotakaki"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
